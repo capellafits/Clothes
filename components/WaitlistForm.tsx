27b1d0cf112deeb46af896e87361cc5fc8c1ba33
capellafits <teamcapellafits@gmail.com>
2026-04-22 10:51:46 -0400

    Create WaitlistForm.tsx

---
+++ components/WaitlistForm.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+export default function WaitlistForm() {
+  return (
+    <section className="w-full bg-black py-16 px-4 flex flex-col items-center justify-center">
+      <div className="klaviyo-form-RbbbzQ" />
+    </section>
+  )
+}
