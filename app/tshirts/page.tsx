53f52c8da9c21c7d10056b98ab36c4d709d2b5bf
capellafits <teamcapellafits@gmail.com>
2026-09-01 16:50:15 -0400

    Add files via upload

--- app/tshirts/page.tsx
+++ app/tshirts/page.tsx
@@ -1,6 +1,7 @@
 // app/tshirts/page.tsx
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
+import CategoryNav from '@/components/CategoryNav';
 import ProductGrid from '@/components/ProductGrid';
 import { fetchProductsByCollection, type Country } from '@/lib/shopify';
 import Link from 'next/link'; 
@@ -32,7 +33,11 @@ export default async function TShirtsPage({
       <Header />
 
       {/* Spacer for fixed header */}
-      <div className="h-20 sm:h-24 mb-6 sm:mb-10"></div>
+      <div className="h-[60px] sm:h-[84px]"></div>
+
+      <div className="max-w-7xl mx-auto pt-2 sm:pt-4">
+        <CategoryNav active="tshirts" country={country} />
+      </div>
 
       {/* Products Section */}
       <ProductGrid
